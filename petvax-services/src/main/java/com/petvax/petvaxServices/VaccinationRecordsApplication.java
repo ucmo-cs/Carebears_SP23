2a7737909a5ce20c9d848ade073d51e847b985ca
Newman <newmancorey@ad.garmin.com>
2023-02-24 18:05:22 -0600

    Created vaccinationRecordsApplication

---
+++ petvax-services/src/main/java/com/petvax/petvaxServices/VaccinationRecordsApplication.java
@@ -0,0 +1,12 @@
+package com.petvax.petvaxServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationRecordsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationRecordsApplication.class, args);
+    }
+
+}
