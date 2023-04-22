81ae1d0cbc6759435918725850721287274285c4
cmn56690 <newmancorey@ad.garmin.com>
2023-04-22 09:35:34 -0500

    Added ability to add bearer token in swagger ui

--- petvax-services/src/main/java/com/petvax/petvaxServices/OpenAPIDocumentationConfig.java
+++ petvax-services/src/main/java/com/petvax/petvaxServices/OpenAPIDocumentationConfig.java
@@ -1,8 +1,14 @@
 package com.petvax.petvaxServices;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +21,18 @@ public class OpenAPIDocumentationConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        SecurityScheme bearerToken = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
                 .info(new Info().title("petvax-services REST Services")
                         .contact(new Contact()
                                 .name("IT")
                                 .email(contactEmail))
-                );
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth", bearerToken));
     }
 }
