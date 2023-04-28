eb1ae8c69ab6d9ef9d076e2c23df525bcfe64ceb
Luis Grados <luis.grados@outlook.es>
2023-04-28 14:50:24 -0500

    Modified Add component to show real list of vaccination from pet rather than db.json list of vaccines

--- petvax-app/petvax-app/src/app/components/wallet-add/wallet-add.component.ts
+++ petvax-app/petvax-app/src/app/components/wallet-add/wallet-add.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Vaccine } from '../wallet-page/wallet-modal';
 import { WalletsService } from 'src/app/services/wallet.service';
 import { CookieService } from 'ngx-cookie-service';
+import { VaccinationRecord } from '../vaccination-page/vaccinationRecord';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-wallet-add',
@@ -12,13 +14,17 @@ import { CookieService } from 'ngx-cookie-service';
 })
 
 export class WalletAddComponent {
-  urlWallet = 'http://localhost:3000/wallets';
-  urlVaccines = 'http://localhost:3000/vaccines';
+  //urlWallet = 'http://localhost:3000/wallets';
+  //urlVaccines = 'http://localhost:3000/vaccines';
+  datePipe = new DatePipe('en-US');
+  urlVaccines = `http://localhost:8080/petvax-services/vaccinationRecord`;
   newWalletName: string = "";
   newWalletPurpose: string = "";
-  allVaccines: Vaccine[] = [];
-  availableVaccines: Vaccine[] = [...this.allVaccines];
-  selectedVaccines: Vaccine [] = [];
+  allVaccines: VaccinationRecord[] = [];
+  availableVaccines: VaccinationRecord[] = [...this.allVaccines];
+  selectedVaccines: VaccinationRecord [] = [];
+  private token = '';
+  private cookieValue = '';
 
   constructor(
     private cookieService: CookieService,
@@ -29,9 +35,26 @@ export class WalletAddComponent {
   
   // Code fetches data from the db.json and assigns it to a local variable wallets that can be used in the component's template.
   ngOnInit(): void {
-    
-    this.http.get<Vaccine[]>(this.urlVaccines).subscribe((data) => { this.allVaccines = data; });
+    this.token = localStorage.getItem('token') || '';
+    this.cookieValue = this.cookieService.get('petId');
+    this.setupDataSource();
+    // this.http.get<Vaccine[]>(this.urlVaccines).subscribe((data) => { this.allVaccines = data; });
     this.getVaccinesList();
+  }
+
+  setupDataSource(): void {
+    const httpOtions = {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.token}`,
+        'Content-Type': 'application/json',
+        'Cookie': `petId=${this.cookieValue}`
+      }),
+      withCredentials: true
+    };
+
+    this.http.get<VaccinationRecord[]>(this.urlVaccines, httpOtions).subscribe((data: any) => {
+        this.allVaccines = data;
+    });
   }
 
   // Function to get wallets from URL. Used in HTML. 
